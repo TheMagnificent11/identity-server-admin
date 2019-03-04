250f6ff539d0c089299df79006c05f24fcbd681c
Saji Weerasingham <saji.weerasingham@outlook.com>
2019-03-05 08:35:11 +1000

    added manage users client claim to admin client

---
+++ IdentityServer.Common/Constants/Claims/AdminClientClaims.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityServer.Common.Constants.Claims
+{
+    public static class AdminClientClaims
+    {
+        public const string ManageUsers = "identity-admin:manage-users";
+    }
+}
