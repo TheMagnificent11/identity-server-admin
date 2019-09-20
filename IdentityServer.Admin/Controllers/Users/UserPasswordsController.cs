ca1d9cb6677535af4692f790466b80888c5e27a6
Saji Weerasingham <saji.weerasingham@outlook.com>
2019-09-21 01:58:02 +1000

    generate http client using nswag

---
+++ IdentityServer.Admin/Controllers/Users/UserPasswordsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using IdentityServer.Admin.Authorization;
+using IdentityServer.Common.Constants;
+using IdentityServer.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Admin.Controllers.Users
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Policy = Policies.ManageUsers)] // TODO: allows users to set their own password
+    public sealed class UserPasswordsController : BaseUserController
+    {
+        public UserPasswordsController(UserManager<User> userManager)
+            : base(userManager)
+        {
+        }
+
+        [HttpPut(Name = "UpdatePasswordForUser")]
+        [Route("{email}/password")]
+        [Consumes(ContentTypes.ApplicationJson)]
+        [Produces(ContentTypes.ApplicationJson)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400, Type = typeof(ValidationProblemDetails))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> PutPassword([FromRoute]string email, [FromBody]UpdateUserPasswordRequest request)
+        {
+            var user = await this.UserManager.FindByNameAsync(email);
+            if (user == null) return this.NotFound();
+
+            var result = await this.UserManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+            return this.ConvertIdentityResultToResponse(result);
+        }
+    }
+}
--- IdentityServer.Admin/Startup.cs
+++ IdentityServer.Admin/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Sgw.KebabCaseRouteTokens;
 
 namespace IdentityServer.Admin
 {
@@ -65,8 +66,13 @@ namespace IdentityServer.Admin
 
             services.AddAutoMapper(GetMappingAssemblies());
 
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options
+                    .Conventions
+                    .Add(new KebabCaseRouteTokenReplacementControllerModelConvention());
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
