0a1fc221e35aaf0632f318767c46b45b3e515713
Saji Weerasingham <saji.weerasingham@outlook.com>
2020-01-04 22:30:40 +1000

    fixed db config and removed cors policy

---
+++ IdentityServer.Admin/Configuration/Database.cs
@@ -0,0 +1,19 @@
+﻿using IdentityServer.Data;
+using IdentityServer.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer.Admin.Configuration
+{
+    public static class Database
+    {
+        public static void ConfigureDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+    }
+}
--- IdentityServer.Admin/Startup.cs
+++ IdentityServer.Admin/Startup.cs
@@ -1,23 +1,18 @@
 using System.Reflection;
 using AutoMapper;
 using IdentityServer.Admin.Configuration;
-using IdentityServer.Data;
 using IdentityServer.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace IdentityServer.Admin
 {
     public class Startup
     {
         private const string ApiName = "Identity Admin API";
-        private const string CorsPlolicyName = "CorsPolicy";
 
         public Startup(IConfiguration configuration)
         {
@@ -34,7 +29,6 @@ namespace IdentityServer.Admin
             }
 
             app.UseHttpsRedirection();
-            app.UseCors(CorsPlolicyName);
             app.UseAuthentication();
 
             app.UseRouting();
@@ -49,19 +43,11 @@ namespace IdentityServer.Admin
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = this.Configuration.GetConnectionString("DefaultConnection");
-
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-            services.AddConfigurationStore(connectionString);
-            services.ConfigureCors(CorsPlolicyName);
+            services.ConfigureDatabase(this.Configuration.GetConnectionString("DefaultConnection"));
 
             services.AddAutoMapper(GetMappingAssemblies());
 
             services.AddControllers();
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
 
             services.ConfigureAuthentication(
                 this.Configuration["AuthServer:BaseUrl"],
