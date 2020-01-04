92f2ac745cb602ca33c4c0556619b466a7279ace
Saji Weerasingham <saji.weerasingham@outlook.com>
2020-01-05 05:42:55 +1000

    fixed issue crated when incorrectly resolving build failure

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
@@ -8,7 +9,7 @@ namespace IdentityServer
 {
     public static class Program
     {
-        public static void Main()
+        public static int Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
@@ -25,6 +26,24 @@ namespace IdentityServer
                 //    flushToDiskInterval: TimeSpan.FromSeconds(1))
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
                 .CreateLogger();
+
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
+
+                Log.Information("Starting host...");
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
